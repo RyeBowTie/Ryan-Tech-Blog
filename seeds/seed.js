d32d19e7baaa1db8ed8a4a8ded5d090dc1fdf070
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-06-09 14:17:37 -0400

    seeds files for populating datbase.

---
+++ seeds/seed.js
@@ -0,0 +1,21 @@
+const sequelize = require('../../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./user-data.json');
+const postData = require('./post-data.json');
+const commentData = require('./comment-data.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData);
+
+    await Post.bulkCreate(postData);
+
+    await Comment.bulkCreate(commentData);
+
+
+    process.exit(0);
+};
+
+seedDatabase();
