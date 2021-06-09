d1235a1481e1d96c7e22823f8e9c04e13eaa00aa
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-06-09 14:16:59 -0400

    basic routes including login and logout.

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        if (req.session.logged_in) {
+            const postData = await Comment.create({
+                title: req.body.title,
+                text: req.body.text,
+                user_id: req.session.user_id
+            })
+            res.status(200).json(postData);
+        } else {
+            res.redirect('/login');
+        }
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
