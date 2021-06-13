9df76e9d0f2df06bbe1f929d3f8faf94622cb06e
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-06-13 10:02:18 -0400

    added additional routes.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,14 +4,15 @@ const { Comment } = require('../../models');
 router.post('/', async (req, res) => {
     try {
         if (req.session.logged_in) {
+            console.log(req.session.user_id)
             const postData = await Comment.create({
-                title: req.body.title,
-                text: req.body.text,
+                comment_text: req.body.comment,
+                post_id: req.body.postId,
                 user_id: req.session.user_id
             })
             res.status(200).json(postData);
         } else {
-            res.redirect('/login');
+            res.render('login');
         }
     } catch (err) {
         res.status(400).json(err);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,80 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
+
 router.get('/', async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      const postData = await Post.findAll({
+        include: {
+          model: User,
+          as: 'user'
+        },
+        order:[['updatedAt',  'DESC']]
+      });
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render('home', { posts, logged_in: req.session.logged_in });
+    } else {
+      res.render('login');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
   try{ 
     if (!req.session.logged_in) {
       res.render('login');
     } else if (req.session.logged_in) {
-      res.render('home', {logged_in:req.session.logged_in})
+      res.render('home', { logged_in:req.session.logged_in })
     }
   } catch (err) {
-    console.log('something')
+    res.status(500).json(err);
   }
-  });
+});
+
+router.get('/register', (req, res) => {
+  try {
+    res.render('register');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post', async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      res.render('post',  { logged_in:req.session.logged_in })
+    } else {
+      res.render('login');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   
+router.get('/post/:id', async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      const postData = await Post.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: {
+          model: Comment,
+          order: [['updatedAt',  'DESC']]
+        }
+      })
+      const posts = postData.get({ plain: true});
+      res.render('comment', {posts, logged_in:req.session.logged_in})
+    } else {
+      res.render('login');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
