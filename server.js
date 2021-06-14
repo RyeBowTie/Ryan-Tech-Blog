7c72799516ebc64e0e109576e92ab35489d9659b
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-06-14 16:06:16 -0400

    updated create method for handlebars.

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
